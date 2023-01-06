cbcf882ca28cd3d3fcb289bdb0f0ed36134cd0cc
artemis867 <dev.ronjovi@gmail.com>
2023-01-06 11:40:41 +0800

    develop | added submit button event

--- src/form/Form.tsx
+++ src/form/Form.tsx
@@ -2,6 +2,17 @@ import React from "react";
 import Box from '@mui/material/Box';
 import { TextField } from '@mui/material';
 import Button from '@mui/material/Button';
+
+async function sendMovieRequest(event: any) {
+  event.preventDefault();
+  try {
+    console.log('event sent');
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+
 function Form() {
   return (
     <>
@@ -11,16 +22,19 @@ function Form() {
           maxWidth: "100%",
         }}
       >
-        <TextField fullWidth label="type a movie title" />
-        <Button 
-          sx={{
-            marginTop: "5px",
-            color: "#000000",
-          }} 
-          variant="outlined"
-        >
-          Send
-        </Button>
+        <form onSubmit={sendMovieRequest}>
+          <TextField fullWidth label="type a movie title" name="movieTitle" />
+          <Button 
+            sx={{
+              marginTop: "5px",
+              color: "#000000",
+            }}
+            type="submit"
+            variant="outlined"
+          >
+            Send
+          </Button>
+        </form>
       </Box>
     </>
   )
